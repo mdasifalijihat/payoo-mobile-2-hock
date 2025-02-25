d16d64f3bd17421b2262713f4ed4e851e770a56c
mdasifalijihat <mdasifalijihat@gmail.com>
2025-02-26 00:36:25 +0600

    add date currentdate

--- js/addmoney.js
+++ js/addmoney.js
@@ -1,47 +1,50 @@
-document.getElementById('add-money-btn').addEventListener('click', function(event){
-event.preventDefault();
-  const amount = getInputValueByID("amount");
-  const pin = getInputValueByID('pinNumber');
-  const account = document.getElementById('account-number').value;
-  const mainBalance = getInnerTextByID('main-balance');
-  const selectedBank = document.getElementById('allbank').value;
-  
+document
+  .getElementById("add-money-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const amount = getInputValueByID("amount");
+    const pin = getInputValueByID("pinNumber");
+    const account = document.getElementById("account-number").value;
+    const mainBalance = getInnerTextByID("main-balance");
+    const selectedBank = document.getElementById("allbank").value;
 
-  if(amount < 0){
-    alert('Please valid amount')
-    return; 
-  }else{
-    alert('Successfully!') 
-  }
-
-   
-  if(account.length === 11){
-    if(pin === 1234){
-       const sum = amount + mainBalance ;
-    //    document.getElementById('main-balance').innerText = sum;
-       setInnerTextByIDandValue('main-balance', sum);
-
-      //  
-       const container = document.getElementById('transaction-container');
-        
-
-       const div = document.createElement('div');
-       div.classList.add('bg-red-400')
-
-       div.innerHTML= `
-       <h1 class='text-yellow-300'> Added Money form${selectedBank} </h1>
-       <h3>${amount}</h3>
-        <p> account number: ${account}</p>
-       `
-       container.appendChild(div); 
-
-      // 
-       
-
-    }else{
-        alert('Invalid PIn ')
+    if (amount < 0) {
+      alert("Please valid amount");
+      return;
+    } else {
+      alert("Successfully!");
     }
-  }else{
-    alert('Wrong Account')
-  }
-})
+
+    if (account.length === 11) {
+      if (pin === 1234) {
+        const sum = amount + mainBalance;
+        //    document.getElementById('main-balance').innerText = sum;
+        setInnerTextByIDandValue("main-balance", sum);
+
+        //  date
+        const currentDate = new Date().toLocaleDateString();
+
+        //
+        const container = document.getElementById("transaction-container");
+
+        const div = document.createElement("div");
+        div.classList.add("bg-white");
+
+        div.innerHTML = `
+       <div class="border rounded-lg text-start p-4 ">
+       <h1 class='text-black'> Added Money form: ${selectedBank} </h1>
+       <h3> Amount: ${amount}</h3>
+        <p> account number: ${account}</p>
+        <p> Date: ${currentDate} </p>
+       </div>
+       `;
+        container.appendChild(div);
+
+        //
+      } else {
+        alert("Invalid PIn ");
+      }
+    } else {
+      alert("Wrong Account");
+    }
+  });
